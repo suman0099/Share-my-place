7a834085588a0b0ca06877d57fc42de21e49214b
Suman Mahato <sumanmahato0099@gmail.com>
2020-01-02 10:10:33 +0530

    Google map added

---
+++ src/SharePlace.js
@@ -0,0 +1,58 @@
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+class PlaceFinder {
+    constructor() {
+        const addressForm = document.querySelector("form");
+        const locateUserBtn = document.getElementById("locate-btn");
+
+        locateUserBtn.addEventListener(
+            "click",
+            this.locateUserHandler.bind(this)
+        );
+        addressForm.addEventListener("submit", this.findAddressHandler);
+    }
+
+    selectPlace(coordinates) {
+        if (this.map) {
+            this.map.render(coordinates);
+        } else {
+            this.map = new Map(coordinates);
+        }
+    }
+
+    locateUserHandler() {
+        if (!navigator.geolocation) {
+            alert(
+                "Location feature is not available in your browser or manually enter your address"
+            );
+            return;
+        }
+
+        const modal = new Modal(
+            "loading-modal-content",
+            "Loading location - please wait"
+        );
+        modal.show();
+
+        navigator.geolocation.getCurrentPosition(
+            successResult => {
+                modal.hide();
+                const coordinates = {
+                    lat: successResult.coords.latitude,
+                    lng: successResult.coords.longitude
+                };
+                this.selectPlace(coordinates);
+            },
+            error => {
+                modal.hide();
+                alert(
+                    "Could not locate you unfortunately. Please enter an address manually"
+                );
+            }
+        );
+    }
+
+    findAddressHandler() {}
+}
+
+const placeFinder = new PlaceFinder();
